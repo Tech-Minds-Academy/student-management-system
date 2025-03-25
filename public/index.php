e2f6dee54f091976b7c7add2aa6c51051615e781
Godfreyjr14 <juliusgodfrey58@gmail.com>
2025-03-25 12:36:36 +0100

    Refactor Project

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/core/Router.php';
-$router = new Router();
-
-require_once __DIR__ . '/../app/routes/web.php';
-$router->dispatch($_SERVER['REQUEST_URI']);
-
-?>
